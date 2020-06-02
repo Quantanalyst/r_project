de70aea2df6224c12865aec648231219dd6a09c4
Saeed Mohajeryami,PhD <smohajer85@gmail.com>
2020-06-02 14:05:09 -0700

    add some required packages to install file

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+
 set -o errexit
+# set -o errexit causes the script to exit immediately if a pipeline sets a non-zero status (exit code). This provides a basic mechanism to ensure that scripts fail unless you intended to ignore a failure.
+
+# Required for many packages
+sudo apt-get install -y libxml2-dev libcurl4-openssl-dev libssl-dev
+
 Rscript "install.R"
